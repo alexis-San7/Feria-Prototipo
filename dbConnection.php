a2a1c338db00bdfdf083a72d6dba7606dcc29275
tatiavane <ia.tatiavane@gmail.com>
2023-11-30 00:48:59 -0600

    conection

--- dbConnection.php
+++ dbConnection.php
@@ -3,6 +3,7 @@ $databaseHost = 'localhost';
 $databaseName = 'prototiposprueba';
 $databaseUsername = 'root';
 $databasePassword = '';
+$port = '3307';
 
 // Open a new connection to the MySQL server
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName, $port); 
