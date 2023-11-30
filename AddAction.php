9bbb9bf4b49a753a13fb0a641dd57fb130db90d5
tatiavane <ia.tatiavane@gmail.com>
2023-11-30 00:31:46 -0600

    sepudo wiii

--- AddAction.php
+++ AddAction.php
@@ -8,53 +8,30 @@
 // Include the database connection file
 require_once("dbConnection.php");
 
-if (isset($_POST['submit'])) {
-	// Escape special characters in string for use in SQL statement	
-	$nombre = mysqli_real_escape_string($mysqli, $_POST['nombre']);
-	$apellido = mysqli_real_escape_string($mysqli, $_POST['apellido']);
-	$nombreusuario = mysqli_real_escape_string($mysqli, $_POST['nombreusuario']);
-    $correo = mysqli_real_escape_string($mysqli, $_POST['correo']);
-    $telefono = mysqli_real_escape_string($mysqli, $_POST['telefono']);
-    $contraseña = mysqli_real_escape_string($mysqli, $_POST['contraseña']);
-		
-	// Check for empty fields
-	if (empty($nombre) || empty($apellido) || empty($nombreusuario) || empty($correo) || empty($telefono) || empty($contraseña)) {
-		if (empty($nombre)) {
-			echo "<font color='red'>Name field is empty.</font><br/>";
-		}
-		
-		if (empty($apellido)) {
-			echo "<font color='red'>Age field is empty.</font><br/>";
-		}
-		
-		if (empty($nombreusuario)) {
-			echo "<font color='red'>Email field is empty.</font><br/>";
-		}
-        if (empty($correo)) {
-			echo "<font color='red'>Name field is empty.</font><br/>";
-		}
-		
-		if (empty($telefono)) {
-			echo "<font color='red'>Age field is empty.</font><br/>";
-		}
-		
-		if (empty($contraseña)) {
-			echo "<font color='red'>Email field is empty.</font><br/>";
-		}
-		
-		// Show link to the previous page
-		echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
-	} else { 
-		// If all the fields are filled (not empty) 
-
-		// Insert data into database
-		$result = mysqli_query($mysqli, "INSERT INTO usuario (`id`,`nombre`, `apellido`, `nombreusuario`, `correo`, `telefono`, , `contraseña`) VALUES (null,'$nombre', '$apellido', '$nombreusuario', '$correo', '$telefono', '$contraseña')");
-		
-		// Display success message
-		echo "<p><font color='green'>Data added successfully!</p>";
-		echo "<a href='index.php'>View Result</a>";
-	}
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
 }
-?>
+	// Escape special characters in string for use in SQL statement	
+
+	$nombre = $_POST['nombre'];
+	$apellido = $_POST['apellido'];
+	$nombreusuario = $_POST['nombreusuario'];
+	$correo = $_POST['correo'];
+	$telefono = $_POST['telefono'];
+	$contraseña = $_POST['contraseña'];
+	
+	
+	// Insertar datos en la base de datos
+	$sql = "INSERT INTO usuario (`nombre`, `apellido`, `nombreusuario`, `correo`, `telefono` `contraseña`) VALUES ('$nombre', '$apellido', '$nombreusuario', '$correo', '$telefono' , '$contraseña')";
+	
+	if ($conn->query($sql) === TRUE) {
+		echo "Datos insertados correctamente";
+	} else {
+		echo "Error al insertar datos: " . $conn->error;
+	}
+	
+	// Cerrar la conexión
+	$conn->close();
+	?>
 </body>
 </html>
--- dbConnection.php
+++ dbConnection.php
@@ -1,8 +1,23 @@
 <?php
-$databaseHost = 'localhost';
-$databaseName = 'prototiposprueba';
-$databaseUsername = 'root';
-$databasePassword = '';
+$servername = "localhost"; 
+$username = "root";
+$password = "";
+$database = "mibd";
+$port = "3307";
 
-// Open a new connection to the MySQL server
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database, $port);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+echo "Conexión exitosa";
+
+// Aquí puedes realizar consultas a la base de datos o ejecutar otras operaciones
+
+// Cerrar la conexión al finalizar
+$conn->close();
+?>
